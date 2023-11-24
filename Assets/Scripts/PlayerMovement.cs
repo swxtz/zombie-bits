18a0e9c92662b5ef7ea6841d817267516db843cc
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-24 20:54:04 -0300

    feat: create rotation funcionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed;
+    public float sensi;
+
+    // Key WASD Our Arrows
     private float horizontalInput;
     private float verticalInput;
 
+    // Mouse Axis
+    private float xMouse;
+    private float yMouse;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     
@@ -20,8 +28,15 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        xMouse = Input.GetAxis("Mouse X") * sensi;
+        yMouse = Input.GetAxis("Mouse Y") * sensi;
+
         transform.Translate(Vector3.left * horizontalInput * Time.deltaTime * speed);
-        transform.Translate(Vector3.fwd * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        Quaternion rotation = Quaternion.Euler(xMouse, yMouse, 0);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed);
 
     }
 }
