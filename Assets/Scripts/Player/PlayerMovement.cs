3d17d54f720502cd901358e656373f27c711adbb
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-24 21:49:42 -0300

    feat: create player prefab

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -16,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     private float xMouse;
     private float yMouse;
 
+    private float xRotation;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -28,15 +31,25 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        xMouse = Input.GetAxis("Mouse X") * sensi;
-        yMouse = Input.GetAxis("Mouse Y") * sensi;
-
         transform.Translate(Vector3.left * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
-        Quaternion rotation = Quaternion.Euler(xMouse, yMouse, 0);
+    //}
 
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed);
+    //private void LateUpdate()
+    //{
+        xMouse = Input.GetAxis("Mouse X") * sensi;
+        yMouse = Input.GetAxis("Mouse Y") * sensi;
 
+        xRotation -= yMouse;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        //Quaternion rotation = Quaternion.Euler(xMouse, yMouse, 0);
+
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * xMouse);
     }
 }
