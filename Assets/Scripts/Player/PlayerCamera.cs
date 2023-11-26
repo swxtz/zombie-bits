4da4763dc314960c7f859a91acbea1cddc8d488b
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-25 21:33:16 -0300

    feat: transfer cursor lock mode from player movement script to player camera script

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -31,4 +31,10 @@ public class PlayerCamera : MonoBehaviour
         // https://www.youtube.com/watch?time_continue=79&v=CxI2OBdhLno&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2F&source_ve_path=MzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMzY4NDIsMjg2NjY&feature=emb_logo
 
     }
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 }
