3ee0fa9d3ad71203c5e43c51ca59920360de3d7d
Gustavo Mendonça <alf4r6@gmail.com>
2024-01-15 18:01:35 -0300

    feat: create weapon system

---
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            print("Hit" + collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+}
