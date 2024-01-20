8826eff70729e22d608ae1f95c15e1010daa53ed
Gustavo Mendonça <alf4r6@gmail.com>
2024-01-20 14:11:51 -0300

    feat: create advanced main menu

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -6,19 +6,20 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [SerializeField]
-    private string initialLevelName;
+    [SerializeField] private string initialLevelName;
 
-    [SerializeField]
-    private AudioSource audioSource;
-    [SerializeField]
-    private float audioVolume = 0.10f;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float audioVolume = 0.10f;
 
-    [SerializeField]
-    private Button playButton;
-    [SerializeField]
-    private Button exitButton;
+    [Header("Menu Wrappers")]
+    [SerializeField] private GameObject mainMenuWrapper;
+    [SerializeField] private GameObject optionsWrapper;
 
+    [Header("Main Menu Buttons")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button exitButton;
+    
 
     private void Awake()
     {
@@ -26,12 +27,14 @@ public class MainMenuManager : MonoBehaviour
         audioSource.Play();
 
         playButton.onClick.AddListener(onClickPlayGame);
-        playButton.onClick.AddListener(onClickExitGame);
+        exitButton.onClick.AddListener(onClickExitGame);
+        optionsButton.onClick.AddListener(onClickOptionsButton);
+
     }
 
     private void onClickPlayGame()
    {
-        SceneManager.LoadScene(initialLevelName);
+        SceneManager.LoadSceneAsync(initialLevelName);
         PauseMenu.StartGameFromMenu();
    }
 
@@ -39,5 +42,10 @@ public class MainMenuManager : MonoBehaviour
     {
         Debug.Log("Saindo...");
         Application.Quit();
+    }
+
+    private void onClickOptionsButton()
+    {
+        Debug.Log("Opções");
     }
 }
